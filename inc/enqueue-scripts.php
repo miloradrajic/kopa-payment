6fe107d68cb9cb788f158bca0e2a1bca40791be0
milorad.rajic <milorad.rajic@tp.rs>
2024-09-30 16:21:30 +0200

    Updated validation library

--- inc/enqueue-scripts.php
+++ inc/enqueue-scripts.php
@@ -1,15 +1,17 @@
-<?php 
+<?php
+
 /**
  * Adding scripts and styles
  */
-function enqueue_kopa_scripts() {
+function enqueue_kopa_scripts()
+{
   // Enqueue scripts and styles on checkout
   if (is_checkout() && !is_wc_endpoint_url()) {
-    wp_enqueue_script('jquery-validate', KOPA_PLUGIN_URL .'js/inc/jquery.validate.min.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('jquery-validate-additional', KOPA_PLUGIN_URL .'js/inc/additional-validate-methods.min.js', array('jquery','jquery-validate'), '1.0', true);
-    wp_enqueue_script('crypto-js', KOPA_PLUGIN_URL .'js/inc/crypto-js.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('socket-io', KOPA_PLUGIN_URL .'js/inc/socket.io.js', array('jquery'), '1.0', true);
-    wp_enqueue_script('ajax-checkout', KOPA_PLUGIN_URL .'js/kopa-scripts.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('jquery-validate', KOPA_PLUGIN_URL . 'js/inc/jquery.validate.min.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('jquery-validate-additional', KOPA_PLUGIN_URL . 'js/inc/additional-methods.min.js', array('jquery', 'jquery-validate'), '1.0', true);
+    wp_enqueue_script('crypto-js', KOPA_PLUGIN_URL . 'js/inc/crypto-js.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('socket-io', KOPA_PLUGIN_URL . 'js/inc/socket.io.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('ajax-checkout', KOPA_PLUGIN_URL . 'js/kopa-scripts.js', array('jquery'), '1.0', true);
 
     // Pass the necessary variables to the JavaScript file
     wp_localize_script('ajax-checkout', 'ajax_checkout_params', array(
@@ -25,12 +27,11 @@ function enqueue_kopa_scripts() {
       'paymentErrorMessageFor3D' => __('There has been an error with payment', 'kopa-payment'),
     ));
 
-    wp_enqueue_style( 'kopa-styles', KOPA_PLUGIN_URL .'css/kopa-styles.css' );
-
+    wp_enqueue_style('kopa-styles', KOPA_PLUGIN_URL . 'css/kopa-styles.css');
   }
   // Enqueue script on My Account page
   if (is_account_page() && !is_wc_endpoint_url() && is_user_logged_in()) {
-    wp_enqueue_script('kopa-my-account', KOPA_PLUGIN_URL .'js/kopa-my-account-scripts.js', array('jquery'), '1.0', true);
+    wp_enqueue_script('kopa-my-account', KOPA_PLUGIN_URL . 'js/kopa-my-account-scripts.js', array('jquery'), '1.0', true);
     wp_localize_script('kopa-my-account', 'ajax_my_account_params', array(
       'ajaxurl'   => admin_url('admin-ajax.php'),
       'security'  => wp_create_nonce('ajax-my-account-nonce'),
@@ -40,4 +41,3 @@ function enqueue_kopa_scripts() {
   }
 }
 add_action('wp_enqueue_scripts', 'enqueue_kopa_scripts');
-
